714ddf6be7b50c4af8c1137dc5a987d93163a21e
harry6396 <harrytyagi2371@gmail.com>
2024-11-29 21:34:13 +0530

    Initial service setup

---
+++ src/test/java/com/swadeshitech/prodhub/ProdhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swadeshitech.prodhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
